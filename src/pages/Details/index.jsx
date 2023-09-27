fd4166f45265aecec8828363c6b2d33cf2375200
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-09-27 21:03:04 +0100

    finish details interface

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -9,19 +9,36 @@ export function Details() {
   return (
     <Container>
       <Header />
+
       <main>
         <Content>
-          <ButtonText title={'Excluir nota'} />
+          <ButtonText title={'Remove Note'} />
 
-          <Section title={'Links Uteis'}>
+          <h1>Introduction to React</h1>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularized in the 1960s
+            with the release of Let sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldo
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+
+          <Section title={'Utils Links'}>
             <Links>
               <li>
-                <a href="#">Link1</a>
+                <a href="#">https://Clemilsonazevedo.github.io/Devlinks</a>
+                <a href="#">https://github.com/clemilsonazevedo</a>
+                <a href="#">https://clemilsonazevedo.dev</a>
               </li>
             </Links>
           </Section>
 
-          <Section title={'Marcadores'}>
+          <Section title={'Marks'}>
             <Tag title={'express'} />
             <Tag title={'Notes'} />
           </Section>
